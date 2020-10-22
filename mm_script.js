ece67f4c3606e20327337366e0b76a990fa06e49
Diamond <me@diamondsystems.org>
2020-10-22 17:38:25 +0500

    bugs fixed

--- mm_script.js
+++ mm_script.js
@@ -2,7 +2,7 @@
 // @name EXT YouTube timer
 // @author DiamondSystems
 // @license GPLv3
-// @version 1.002
+// @version 1.003
 // @include https://www.youtube.com/*
 // @grant none
 // @run-at document-start
@@ -13,13 +13,14 @@
  * Settings
  * ------------------------------------------
  **/
-let workingHours = {
+const workingHours = {
     from: 6,
     to:   18
 };
-let dailyLimitInMinutes = 120;
-let reminderIntervalInSeconds = 600;
-let checkIntervalInSeconds = 10;
+const dailyLimitInMinutes = 120;
+const reminderIntervalInSeconds = 600;
+const checkIntervalInSeconds = 10;
+const redirectUrl = "https://www.google.com/search?q=motivation+to+work";
 
 
 /**
@@ -33,7 +34,7 @@ if (confirm("You're not working?"))
     const date = new Date();
     const currentHour = date.getHours();
 
-    let   isStop = localStorage.getItem('stop');
+    let isStop = localStorage.getItem('stop');
     if (isStop === null)
         isStop = 0;
 
@@ -82,21 +83,28 @@ if (confirm("You're not working?"))
     }
 }
 
+function stopYoutube()
+{
+    if (document.body !== null)
+    {
+        document.body.style.background = '#181818';
+        document.body.style.color = '#fff';
+        document.body.style.fontSize = '60px';
+        document.body.innerHTML = '<div style="text-align:center">:)</div>';
+
+        clearInterval(intervalId);
+        setTimeout(redirectFromYoutube, 1000);
+    }
+    else
+        redirectFromYoutube();
+}
+
 function pausePlayer()
 {
     document.getElementsByClassName("html5-main-video")[0].click();
 }
 
-function stopYoutube()
+function redirectFromYoutube()
 {
-    clearInterval(intervalId);
-
-    document.body.style.background = '#181818';
-    document.body.style.color = '#fff';
-    document.body.style.fontSize = '60px';
-    document.body.innerHTML = '<div style="text-align:center">:)</div>';
-
-    setTimeout(function() {
-        document.location.href = "https://www.google.com/search?q=motivation+to+work";
-    }, 1000);
+    document.location.href = redirectUrl;
 }
